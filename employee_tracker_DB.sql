e38f2487645536f6ad6f410e7b965bd7cd229d44
Patrick Ceriale <patceriale@gmail.com>
2020-10-11 12:53:35 -0700

    consolidated sql files and added more funcionality to js

--- employee_tracker_DB.sql
+++ employee_tracker_DB.sql
@@ -8,8 +8,22 @@ CREATE TABLE employees(
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
   role_id INT,
-  manager_id INT,
+  manager_id VARCHAR(100),
   PRIMARY KEY (empl_id)
+);
+
+CREATE TABLE roles(
+  role_id INT NOT NULL AUTO_INCREMENT,
+  role_title VARCHAR(100) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (role_id)
+);
+
+CREATE TABLE departments(
+  dept_id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (dept_id)
 );
 
 INSERT INTO employees (first_name, last_name, role_id) VALUES ("Patrick", "Ceriale", 1);
--- index.js
+++ index.js
@@ -41,8 +41,75 @@ function start() {
 }
 
 function addEmployee() {
-  console.log("Employee has been added!");
-  start();
+  inquirer
+    .prompt([
+      // Questions for employees table
+      {
+        name: "firstName",
+        type: "input",
+        message: "What is the first name of the employee?",
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "What is the last name of the employee?",
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "Who is the employee's manager?",
+      },
+      // Questions for roles table
+      {
+        name: "roleTitle",
+        type: "input",
+        message: "What is the employee's role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the employee's salary?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+      // Question for department
+      {
+        name: "department",
+        type: "input",
+        message: "What department does the employee work for?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          manager_id: answer.manager,
+          // role_id: comes from roles
+          // manager_id: comes from
+        },
+        "INSERT INTO roles SET ?",
+        {
+          role_title: answer.roleTitle,
+          salary: answer.salary,
+        },
+        "INSERT INTO departments SET ?",
+        {
+          dept_name: answer.department,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Employee has been added!");
+          // re-prompt the user for if they want to add, view or update employee
+          start();
+        }
+      );
+    });
 }
 function viewEmployees() {
   console.log("Here is your current staff!");
