47557c2b8ea69f21aab70a0fadffe0bd0daf92c8
Patrick Ceriale <patceriale@gmail.com>
2020-10-11 16:27:14 -0700

    trying to get add function in its own js file

---
+++ addEmployee.js
@@ -0,0 +1,74 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const addEmployee = addEmp();
+
+// Function to add new employee
+function addEmp() {
+  inquirer
+    .prompt([
+      // Questions for employees table
+      {
+        name: "firstName",
+        type: "input",
+        message: "What is the first name of the employee?",
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "What is the last name of the employee?",
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "Who is the employee's manager?",
+      },
+      // Questions for roles table
+      {
+        name: "roleTitle",
+        type: "input",
+        message: "What is the employee's role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the employee's salary?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          console.log(" is not a number. Please enter a number.");
+          return false;
+        },
+      },
+      // Question for department
+      {
+        name: "department",
+        type: "input",
+        message: "What department does the employee work for?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query("INSERT INTO employees SET ?", {
+        first_name: answer.firstName,
+        last_name: answer.lastName,
+        manager_id: answer.manager,
+        // role_id: comes from roles
+        // manager_id: comes from
+      });
+      connection.query("INSERT INTO roles SET ?", {
+        role_title: answer.roleTitle,
+        salary: answer.salary,
+      });
+      connection.query("INSERT INTO departments SET ?", {
+        dept_name: answer.department,
+      }),
+        function (err) {
+          if (err) throw err;
+        };
+      // re-prompt the user for if they want to add/view/update employee
+      console.log("Employee has been added!");
+      //   start();
+    });
+}
+
+module.exports = addEmployee;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const addEmployee = require("./addEmployee");
 
 // Create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -29,7 +30,9 @@ function start() {
     })
     .then(function (answer) {
       if (answer.addViewUpdate === "ADD") {
-        addEmployee();
+        // TODO: figure out how to make this work
+        addEmployee;
+        // start();
       } else if (answer.addViewUpdate === "VIEW") {
         viewEmployees();
       } else if (answer.addViewUpdate === "UPDATE") {
@@ -40,81 +43,83 @@ function start() {
     });
 }
 
-function addEmployee() {
-  inquirer
-    .prompt([
-      // Questions for employees table
-      {
-        name: "firstName",
-        type: "input",
-        message: "What is the first name of the employee?",
-      },
-      {
-        name: "lastName",
-        type: "input",
-        message: "What is the last name of the employee?",
-      },
-      {
-        name: "manager",
-        type: "input",
-        message: "Who is the employee's manager?",
-      },
-      // Questions for roles table
-      {
-        name: "roleTitle",
-        type: "input",
-        message: "What is the employee's role?",
-      },
-      {
-        name: "salary",
-        type: "input",
-        message: "What is the employee's salary?",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-      // Question for department
-      {
-        name: "department",
-        type: "input",
-        message: "What department does the employee work for?",
-      },
-    ])
-    .then(function (answer) {
-      connection.query(
-        "INSERT INTO employees SET ?",
-        {
-          first_name: answer.firstName,
-          last_name: answer.lastName,
-          manager_id: answer.manager,
-          // role_id: comes from roles
-          // manager_id: comes from
-        },
-        "INSERT INTO roles SET ?",
-        {
-          role_title: answer.roleTitle,
-          salary: answer.salary,
-        },
-        "INSERT INTO departments SET ?",
-        {
-          dept_name: answer.department,
-        },
-        function (err) {
-          if (err) throw err;
-          console.log("Employee has been added!");
-          // re-prompt the user for if they want to add, view or update employee
-          start();
-        }
-      );
-    });
-}
+// // Function to add new employee
+// function addEmployee() {
+//   inquirer
+//     .prompt([
+//       // Questions for employees table
+//       {
+//         name: "firstName",
+//         type: "input",
+//         message: "What is the first name of the employee?",
+//       },
+//       {
+//         name: "lastName",
+//         type: "input",
+//         message: "What is the last name of the employee?",
+//       },
+//       {
+//         name: "manager",
+//         type: "input",
+//         message: "Who is the employee's manager?",
+//       },
+//       // Questions for roles table
+//       {
+//         name: "roleTitle",
+//         type: "input",
+//         message: "What is the employee's role?",
+//       },
+//       {
+//         name: "salary",
+//         type: "input",
+//         message: "What is the employee's salary?",
+//         validate: function (value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           console.log(" is not a number. Please enter a number.");
+//           return false;
+//         },
+//       },
+//       // Question for department
+//       {
+//         name: "department",
+//         type: "input",
+//         message: "What department does the employee work for?",
+//       },
+//     ])
+//     .then(function (answer) {
+//       connection.query("INSERT INTO employees SET ?", {
+//         first_name: answer.firstName,
+//         last_name: answer.lastName,
+//         manager_id: answer.manager,
+//         // role_id: comes from roles
+//         // manager_id: comes from
+//       });
+//       connection.query("INSERT INTO roles SET ?", {
+//         role_title: answer.roleTitle,
+//         salary: answer.salary,
+//       });
+//       connection.query("INSERT INTO departments SET ?", {
+//         dept_name: answer.department,
+//       }),
+//         function (err) {
+//           if (err) throw err;
+//         };
+//       // re-prompt the user for if they want to add/view/update employee
+//       console.log("Employee has been added!");
+//       start();
+//     });
+// }
+
+// Function to view current employees
 function viewEmployees() {
+  // TODO: console.table? employee_tracker_DB
   console.log("Here is your current staff!");
   start();
 }
+
+// Function to update employees
 function updateEmployees() {
   console.log("Employee has been updated!");
   start();
