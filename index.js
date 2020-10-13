8b2ba264c9b4afb297408fc80921ada3760ef693
Patrick Ceriale <patceriale@gmail.com>
2020-10-13 15:55:27 -0700

    still having promise issues

--- index.js
+++ index.js
@@ -145,48 +145,69 @@ function viewAll() {
 
 function viewDepartments() {
   console.log("Here are the departments");
-  connection.query(
-    "SELECT * FROM departments", //TODO: Join with employees
-    function (err, res) {
-      if (err) throw err;
-      inquirer
-        .prompt({
-          name: "deptChoice",
-          type: "rawlist",
-          message: "Which department would you like to view?",
-          choices: function () {
-            const choiceArray = [];
-            for (let i = 0; i < res.length; i++) {
-              choiceArray.push(res[i].department);
-            }
-            return choiceArray;
-          },
-        })
-        .then(function (answer) {
-          let chosenDept;
-          for (var i = 0; i < res.length; i++) {
-            if (res[i].department === answer.deptChoice) {
-              chosenDept = res[i];
-              connection.query("SELECT * FROM departments", function (
-                err,
-                res
-              ) {
-                if (err) throw err;
-
-                console.table(res);
-                start();
-              });
-            }
+  connection.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: "deptChoice",
+        type: "rawlist",
+        message: "Which department would you like to view?",
+        choices: function () {
+          let choiceArray = [];
+          for (let i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].department);
           }
-        });
-    }
-  );
+          return choiceArray;
+        },
+      })
+      .then(function (answer) {
+        let chosenDept;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].department === answer.deptChoice) {
+            chosenDept = res[i];
+            connection.query("SELECT * FROM departments", function (err, res) {
+              if (err) throw err;
+              console.table(res);
+              start();
+            });
+          }
+        }
+      });
+  });
 }
 
 function viewRoles() {
   // Copy viewDept but change to roles
   console.log("Here are the roles");
-  start();
+  connection.query("SELECT * FROM roles", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: "roleChoice",
+        type: "rawlist",
+        message: "Which role would you like to view?",
+        choices: function () {
+          let choiceArray = [];
+          for (let i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].role);
+          }
+          return choiceArray;
+        },
+      })
+      .then(function (answer) {
+        let chosenRole;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].role === answer.roleChoice) {
+            chosenRole = res[i];
+            connection.query("SELECT * FROM roles", function (err, res) {
+              if (err) throw err;
+              console.table(res);
+              start();
+            });
+          }
+        }
+      });
+  });
 }
 
 function viewEmployees() {
@@ -200,6 +221,35 @@ function viewEmployees() {
 
 /////////////////// Function to update employees///////////////////
 function updateEmployees() {
-  console.log("Employee has been updated!");
-  start();
+  connection.query("SELECT * FROM employees", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: "empChoice",
+        message: "What employee would you like to update?",
+        type: "rawlist",
+        choices: function () {
+          let choiceArray = [];
+          for (let i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].employee);
+          }
+          return choiceArray;
+        },
+      })
+      .then(function (answer) {
+        let chosenEmp;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].department === answer.empChoice) {
+            chosenEmp = res[i];
+            connection.query("SELECT * FROM employees", function (err, res) {
+              if (err) throw err;
+              console.table(res);
+              start();
+            });
+          }
+        }
+      });
+    console.log("Employee has been updated!");
+    start();
+  });
 }
